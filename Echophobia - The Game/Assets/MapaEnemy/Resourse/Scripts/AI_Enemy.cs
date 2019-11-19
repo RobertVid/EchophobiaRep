fb0dff0f4ab79834f041301333cbd313640b69fa
RobertVid <33789155+RobertVid@users.noreply.github.com>
2019-11-19 10:47:34 -0600

    Enemigo

---
+++ Echophobia - The Game/Assets/MapaEnemy/Resourse/Scripts/AI_Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AI_Enemy : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject target;
+    public Animator Anim;
+    public void Start()
+    {
+        agent.SetDestination(target.transform.position);
+    }
+
+    public void Update()
+    {
+        Anim.SetFloat("Velocity", agent.velocity.normalized.magnitude);
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+
+
+            agent.isStopped=true;
+            StartCoroutine(AnimAttack());
+    
+    }
+
+    IEnumerator AnimAttack() {
+        Anim.SetTrigger("UsarAtaque");
+        Anim.SetFloat("Ataque", 1);
+        Debug.Log(Anim.GetCurrentAnimatorStateInfo(Anim.GetLayerIndex("Base Layer")).length);
+        yield return new WaitForSeconds(2.7f);
+        Debug.Log(Anim.GetCurrentAnimatorStateInfo(Anim.GetLayerIndex("Base Layer")).length);
+        agent.isStopped = false; ;
+        
+    }
+}
