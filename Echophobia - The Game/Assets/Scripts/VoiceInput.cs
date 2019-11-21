c139043643b46a7f857387a37226a5a8058b6c29
Heidren <heidren_atondo@hotmail.com>
2019-11-21 16:57:53 -0600

    Lo mio de la semana pasada jaja

---
+++ Echophobia - The Game/Assets/Scripts/VoiceInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceInput : MonoBehaviour
+{
+    AudioSource audioSource;
+    AudioClip recordingClip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("PushToTalk"))
+        {
+            StartRecording();
+        }
+        if (Input.GetButtonUp("PushToTalk"))
+        {
+            StopRecording();
+            audioSource.clip = recordingClip;
+            audioSource.Play();
+        }
+    }
+
+    void StartRecording()
+    {
+        recordingClip = Microphone.Start(null, true, 300, 48000);
+        Debug.Log("RECORDING VOICE...");
+    }
+
+    void StopRecording()
+    {
+        Microphone.End(null);
+        Debug.Log("NO LONGER RECORDING VOICE.");
+    }
+}
