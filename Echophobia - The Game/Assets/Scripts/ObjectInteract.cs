3c51e458066a05c6e15b5ef089f6bf03ae460e0d
PacoFMD <pacomonfmd@hotmail.com>
2019-11-25 16:46:30 -0600

    Shader with Triggers

---
+++ Echophobia - The Game/Assets/Scripts/ObjectInteract.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInteract : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+       // Debug.Log(other.name);
+        other.gameObject.GetComponent<Renderer>().material.SetFloat("_Outline", 0.003f);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.gameObject.GetComponent<Renderer>().material.SetFloat("_Outline",0);
+        
+    }
+}
