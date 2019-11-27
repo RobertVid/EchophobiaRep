4a87b1551912d666397d1048db2ab2ed21e6ec07
RobertVid <33789155+RobertVid@users.noreply.github.com>
2019-11-27 13:23:15 -0600

    EnemySteps

---
+++ Echophobia - The Game/Assets/MapaEnemy/Resourse/Scripts/IsGround.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsGround : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool pieIzq=false;
+    public bool pieDer = false;
+    private float lastCheck =0.0f;
+    private void LateUpdate()
+    {
+        if (pieIzq && Time.time>lastCheck+0.1f)
+        {
+            Debug.Log("Piso el píe izquierdo");
+            lastCheck = Time.time;
+        }
+        if (pieDer && Time.time > lastCheck + 0.1f)
+        {
+            Debug.Log("Piso el píe Derecho");
+            lastCheck = Time.time;
+        }
+    }
+   
+}
