f2f2877ffa28f8dd66cba83db7ba82e2f9a8f05a
RobertVid <33789155+RobertVid@users.noreply.github.com>
2019-12-11 10:58:27 -0600

    Anim Chidas y Uso de Raycast y enemigo en escenario v1.0.RDJr

--- Echophobia - The Game/Assets/MapaEnemy/Resourse/Scripts/AI_Enemy.cs
+++ Echophobia - The Game/Assets/MapaEnemy/Resourse/Scripts/AI_Enemy.cs
@@ -5,23 +5,51 @@ using UnityEngine.AI;
 public class AI_Enemy : MonoBehaviour
 {
     public NavMeshAgent agent;
-    public GameObject target;
+    private GameObject target;
     public Animator Anim;
+    private Vector3 point;
+
     public void Start()
     {
-        agent.SetDestination(target.transform.position);
+        target = GameObject.FindGameObjectWithTag("Player");
+        CambiarPunto();
     }
 
     public void Update()
     {
         Anim.SetFloat("Velocity", agent.velocity.normalized.magnitude);
-    }
+
+        float distance = Vector3.Distance(transform.position, point);
+
+        if (distance < 0.5f) {
+            CambiarPunto();
+        }
+
+        RaycastHit hit;
+      
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 100))
+        {
+            if (hit.transform.tag == "Player")
+            {
+                agent.SetDestination(target.transform.position);
+                transform.LookAt(target.transform.position);
+            }
+            else {
+                agent.SetDestination(point);
+            }
+        }
+        }
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             agent.isStopped = true;
             StartCoroutine(AnimAttack());
+            //Muerte
+        }
+
+        if (other.CompareTag("DontWalk")) {
+            CambiarPunto();
         }
     
     }
@@ -34,5 +62,36 @@ public class AI_Enemy : MonoBehaviour
         Debug.Log(Anim.GetCurrentAnimatorStateInfo(Anim.GetLayerIndex("Base Layer")).length);
         agent.isStopped = false; ;
         
+    }
+
+    public float range = 10.0f;
+
+    void CambiarPunto()
+    {
+        bool Enontrado = true;
+        while (Enontrado)
+        {
+            if (RandomPoint(transform.position, range, out point))
+            {
+                agent.SetDestination(point);
+                Enontrado = false;
+            }
+        }
+    }
+
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Vector3 randomPoint = center + Random.insideUnitSphere * range;
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+            {
+                result = hit.position;
+                return true;
+            }
+        }
+        result = Vector3.zero;
+        return false;
     }
 }
