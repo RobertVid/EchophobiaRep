471c9d16dc8ebe915395a34679898c80ef44284d
PacoFMD <pacomonfmd@hotmail.com>
2020-01-17 01:17:01 -0600

    Footprints

--- Echophobia - The Game/Assets/MapaEnemy/Resourse/Scripts/IsGround.cs
+++ Echophobia - The Game/Assets/MapaEnemy/Resourse/Scripts/IsGround.cs
@@ -9,16 +9,47 @@ public class IsGround : MonoBehaviour
     public bool pieDer = false;
     private float lastCheck =0.0f;
     public GameObject objectToSpawn;
+
+
+    public GameObject rightfootprint, leftfootprint;
+    public Transform righfootTransform, leftfootTransform;
+
     private void LateUpdate()
     {
-        if ((pieIzq|| pieDer) && Time.time>lastCheck+0.1f)
+        if ((pieIzq || pieDer) && Time.time > lastCheck + 0.1f)
         {
+            if (pieIzq)
+            {
+                LeftFoot();
+            }
+            if (pieDer)
+            {
+                RightFoot();
+            }
             Debug.Log("Piso el píe izquierdo");
-            lastCheck = Time.time;
+            lastCheck = Time.time;  
             GameObject obj = Instantiate(objectToSpawn, transform.position, transform.rotation);
             obj.GetComponent<SoundwaveBehavior>().Initialize(10, 1);
         }
 
     }
-   
+
+    void LeftFoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(leftfootTransform.position, leftfootTransform.forward, out hit))
+        {
+            Instantiate(leftfootprint, new Vector3(hit.point.x, hit.point.y+0.003f, hit.point.z), Quaternion.LookRotation(hit.normal, leftfootTransform.up));
+        }
+    }
+
+    void RightFoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(righfootTransform.position, righfootTransform.forward, out hit))
+        {
+            Instantiate(rightfootprint, new Vector3(hit.point.x, hit.point.y+0.003f, hit.point.z), Quaternion.LookRotation(hit.normal, righfootTransform.up));
+        }
+    }
+
 }
---
+++ Echophobia - The Game/Assets/Scripts/Footprint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footprint : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject rightfootprint, leftfootprint;
+    public Transform righfootTransform, leftfootTransform;
+
+    
+    void LeftFoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(leftfootTransform.position, leftfootTransform.forward, out hit))
+        {
+            Instantiate(leftfootprint, hit.point, Quaternion.LookRotation(hit.normal, leftfootTransform.up));
+        }
+    }
+
+    void RightFoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(righfootTransform.position,righfootTransform.forward,out hit))
+        {
+            Instantiate(rightfootprint, hit.point, Quaternion.LookRotation(hit.normal, righfootTransform.up));
+        }
+    }
+}
