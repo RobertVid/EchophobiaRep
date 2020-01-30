0f937365f53e2dafdf0c8088ac9575d691fc20f6
PacoFMD <pacomonfmd@hotmail.com>
2020-01-30 00:15:53 -0600

    Script Shader

---
+++ Echophobia - The Game/Assets/Scripts/ExternalSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExternalSound : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponent<AI_Enemy>().EscuchoAlPlyaer();
+        }
+        if (other.tag != "Enemy" && other.tag != "Doctor" && other.tag != "Player" && other.tag != "Gamemanager")
+        {
+            other.gameObject.GetComponent<Renderer>().material.SetFloat("_Outline", 0.003f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag != "Enemy" && other.tag != "Doctor" && other.tag != "Player" && other.tag != "Gamemanager")
+        {
+            other.gameObject.GetComponent<Renderer>().material.SetFloat("_Outline", 0.0f);
+        }
+    }
+}
